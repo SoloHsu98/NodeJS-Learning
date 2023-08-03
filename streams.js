d531d49b412993068ccc1464cacc930d5dc6e630
Solo Hsu <53265779+SoloHsu98@users.noreply.github.com>
2023-08-03 16:07:33 +0630

    pipe method and create package.json file with 2pkg

--- streams.js
+++ streams.js
@@ -16,15 +16,28 @@ server.listen(8000, "127.0.0.1", () => {
 //   });
 // }); // This can cause apps crash
 
+//Using Readable and Writable Stream
 server.on("request", (req, res) => {
   let part = 0;
   let rs = fs.createReadStream("./files/large-file.txt");
   rs.on("data", (chunk) => {
     res.write(chunk);
     part += 1;
+  });
+  rs.on("end", () => {
     res.end(`There is ${part} chunks`);
   });
   res.on("error", (error) => {
     res.end(error.message);
   });
+}); // this cause back pressure (you can see def at stream.txt)
+
+//Using Pipe Method
+
+server.on("request", (req, res) => {
+  let rs = fs.createReadStream("./files/large-file.txt");
+
+  rs.pipe(res); // pipe method is only availabel only on readable stream
 });
+
+console.log("node mon has working");
